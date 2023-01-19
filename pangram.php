dc761d8b652149adde1b517da136bf44af2d7e79
Eddy <eddy@qdata-mg.com>
2023-01-19 11:37:57 +0300

    pangram OK via CHatGPT

--- pangram.php
+++ pangram.php
@@ -2,35 +2,31 @@
 
 /*phrase contenant toutes les lettres de l'alphabet
 et occurence = 1*/
-function detect_pangram($string)
-{
-  #your code here
-  $a_texte = str_split($string);
-  $c = 0;
-  $a_alphabet = range('a','z');//array indicé de a à z ; dernier indice 25
-  foreach ($a_texte as $char)
-  {//pour chaque lettre
-	//on test seulement les lettre de l'alphabet
-	if (in_array(strtolower($char),$a_alphabet))
-	{
-    	foreach ($a_alphabet as $alphabet)
-    	{
-        	if ( strtoupper($char)==strtoupper($alphabet) )
-        	{
-            	$c++;
-            	continue;//passe à la lettre suivante du texte
-        	}
-     	 
-    	}//end foreach alphabet
-   	 
-    	if ($c ==0 ) { return false; } else { $c=0; }  
-	}
- 	 
-  }//end foreach lettre
- 
-  return true;
-}//end fct
+function isPangram2($str) {
+    $alphabet = "abcdefghijklmnopqrstuvwxyz";
+    $str = strtolower($str);
+    $letters = count_chars($str, 1);
+    foreach ($letters as $letter => $count) {
+        if ($count > 1) {
+            return false;
+        }
+    }
+    for ($i = 0; $i < strlen($alphabet); $i++) {
+        if (strpos($str, $alphabet[$i]) === false) {
+            return false;
+        }
+    }
+    return true;
+}
 
-$result4 = detect_pangram("The quick brown fox jumps over the lazy dog.");
-//$result5 = detect_pangram("Abcdefghijklmnopqrstuvwxyz");
-var_dump($result4);
+
+function isPangram($str) {
+    $alphabet = "abcdefghijklmnopqrstuvwxyz";
+    $str = strtolower($str);
+    for ($i = 0; $i < strlen($alphabet); $i++) {
+        if (strpos($str, $alphabet[$i]) === false) {
+            return false;
+        }
+    }
+    return true;
+}
